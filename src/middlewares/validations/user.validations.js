ef68da576ddd5a4fd0556b490eac7f574d32b0d4
Diego Garcia <tu_correo_de_github@example.com>
2025-09-04 10:28:33 -0300

    Se crearon las validaciones para poder CREAR y ACTUALIZAR los usuarios

--- src/middlewares/validations/user.validations.js
+++ src/middlewares/validations/user.validations.js
@@ -1,12 +1,47 @@
-import { body, param } from "express-validator";
-import { UserModel } from "../../models/user.model.js";
+import { body } from "express-validator";
 
 export const createUserValidation = [
- body("username")
-    .notEmpty().withMessage("El campo Username es Obligatorio"),
-
- body("email")
-    .notEmpty().withMessage("el campo Email es obligatorio")
+body("username")
+    .isAlphanumeric()
+    .withMessage("'username' debe ser un string.")
+    .trim()
+    .notEmpty()
+    .withMessage("'username' no puede estar vacío")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("'username' debe tener entre 3 y 20 caracteres"),
+  body("email")
+    .isString()
+    .withMessage("'email' debe ser un string.")
+    .trim()
+    .notEmpty()
+    .withMessage("'email' no puede estar vacío")
+    .isEmail()
+    .withMessage("formato de email no válido.")
+    .isLength({ max: 100 })
+    .withMessage("'email' solo permite 255 carácteres."),
+  body("password")
+    .isString()
+    .withMessage("'password' debe ser un string.")
+    .trim()
+    .notEmpty()
+    .withMessage("'password' no puede estar vacío")
+    .isStrongPassword({ minNumbers: 0, minSymbols: 0 })
+    .withMessage(
+      "'password' debe tener una longitud de 8 caracteres, debe contener al menos una minúscula, al menos una mayúscula, y al menos un número."
+    ),
+  body("role")
+    .optional()
+    .isString()
+    .withMessage("'role' debe ser un string.")
+    .trim()
+    .toLowerCase()
+    .notEmpty()
+    .withMessage("'role' no puede estar vacío")
+    .custom((value) => {
+      if (value != "admin" && value != "user")
+        throw new Error("'role' solo puede tomar los valores 'admin' y 'user'");
+      return true;
+    })
 ];
 
 export const updateUserValidation = [
@@ -27,5 +62,15 @@ export const updateUserValidation = [
   .isLength({ min: 6, max: 225 }).withMessage("La contraseña debe tener entre 6 y 225 caracteres"),
 
   body("role")
-  
+  .optional()
+  .isString().withMessage("El campo 'role' debe ser una cadena de Texto")
+  .trim()
+  .toLowerCase()
+  .notEmpty()
+  .withMessage("El campo 'role' no puede estar vacío")
+  .custom((value) => {
+    if (value != "admin" && value != "user") 
+      throw new Error('el campo "role" solo puede tomar los valores "admin" y/o "user" ');
+    return true;
+  })
 ];
