6f1312461bceb57fb9436f658bf8124937508fd9
Diego Garcia <tu_correo_de_github@example.com>
2025-09-05 12:50:14 -0300

    se módicicó la tabla intermedia de Articles y Tags

--- src/models/ArticlesTags.js
+++ src/models/ArticlesTags.js
@@ -1,23 +1,37 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
-import { TagsModel } from "./tag.model.js";
-import { ArticleModel } from "./article.model.js";
+import {ArticleModel} from "./article.model.js";
+import {TagsModel} from "./tag.model.js";
 
-export const ArticlesTags = sequelize.define("Articles_Tags", {
-    id: {type: DataTypes.INTEGER,
-        primaryKey:true,
-         autoIncrement: true, 
-         unique:true, 
-         allowNull:false
-    }
-,
-} ,{
-     updatedAt:false,
-     createdAt:false
-   }
-)
+export const ArticleTags = sequelize.define("Articles_Tags", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+});
 
-//RELACION N:M   ----  MUCHOS ARTICULOS TIENEN MUCHAS ETIQUETAS Y MUCHAS ETIQUETAS TIENEN MUCHOS ARTICULOS
-ArticleModel.belongsToMany(TagsModel, {through: ArticlesTags, foreignKey: "articles_id", as: "articles"})
+ArticleModel.belongsToMany(TagsModel, {
+  through: ArticleTags,
+  foreignKey: "article_id",
+  as: "tags",
+  onDelete: "CASCADE",
+});
 
-TagsModel.belongsToMany(ArticleModel, {through: ArticlesTags, foreignKey: "tags_id", as: "tags"})
+TagsModel.belongsToMany(ArticleModel, {
+  through: ArticleTags,
+  foreignKey: "tag_id",
+  as: "articles",
+  onDelete: "CASCADE",
+});
+ArticleTags.belongsTo(TagsModel, {
+  foreignKey: "tag_id",
+  as: "tags",
+  onDelete: "CASCADE",
+});
+
+ArticleTags.belongsTo(ArticleModel, {
+  foreignKey: "article_id",
+  as: "articles",
+  onDelete: "CASCADE",
+});
