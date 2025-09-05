7ea589e9f141e69d7cdd4fc04e5772649e9df979
Diego Garcia <tu_correo_de_github@example.com>
2025-09-05 11:57:40 -0300

    Se realizó los controladores para la tabla intermedia de Articles y Tags

---
+++ src/controllers/article_tag.controller.js
@@ -0,0 +1,24 @@
+import {ArticleTag} from "../models/ArticlesTags.js"
+
+export const createArticleTag = async (req, res) => {
+  try {
+    const articleTag = await ArticleTag.create(req.body);
+    return res.status(201).json(articleTag);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteArticleTag = async (req, res) => {
+  const { articleTagId } = req.params;
+  try {
+    const deleted = await ArticleTag.destroy({ where: { articleTagId } });
+    if (!deleted)
+      return res.status(404).json({ message: "La relación no existe" });
+    return res
+      .status(200)
+      .json({ message: "Relación eliminada correctamente" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
